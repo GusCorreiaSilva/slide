9b5d872f4b076562b3b6fef1b151399fbde7f797
Gustavo <gustaaavox@gmail.com>
2025-08-28 16:45:48 -0300

    feat: mouse events added

--- js/slide.js
+++ js/slide.js
@@ -7,19 +7,28 @@ export default class Slide {
 
   onStart(event) {
     event.preventDefault();
-    console.log(this);
+    this.wrapper.addEventListener('mousemove', this.onMove)
   }
 
   onMove(event) {
-    // console.log('moveu');
+  }
+
+  onEnd(event) {
+    console.log('Cabo');
+    this.wrapper.removeEventListener('mousemove', this.onMove)
     
   }
 
+
   addSlideEvents() {
     this.wrapper.addEventListener('mousedown', this.onStart)
+    this.wrapper.addEventListener('mouseup', this.onEnd)
   }
+
   bindEvents() {
     this.onStart = this.onStart.bind(this)
+    this.onMove = this.onMove.bind(this)
+    this.onEnd = this.onEnd.bind(this)
   }
 
   init() {

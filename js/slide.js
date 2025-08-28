39ace60d6e1bd1dd1e2cc9cebc8680a70ce0206a
Gustavo <gustaaavox@gmail.com>
2025-08-28 16:33:13 -0300

    feat: mouse down event

--- js/slide.js
+++ js/slide.js
@@ -2,5 +2,29 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide)
     this.wrapper = document.querySelector(wrapper)
+
+  }
+
+  onStart(event) {
+    event.preventDefault();
+    console.log(this);
+  }
+
+  onMove(event) {
+    // console.log('moveu');
+    
+  }
+
+  addSlideEvents() {
+    this.wrapper.addEventListener('mousedown', this.onStart)
+  }
+  bindEvents() {
+    this.onStart = this.onStart.bind(this)
+  }
+
+  init() {
+    this.bindEvents() 
+    this.addSlideEvents()
+    return this
   }
 }
